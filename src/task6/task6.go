7299f7cac9577d06b90aa5e2f496a47a9768ae47
Evgeniy Kostrika <evgeniy.kostrika@gmail.com>
2017-06-18 17:05:29 +0300

    cool commit!

--- src/task6/task6.go
+++ src/task6/task6.go
@@ -1,14 +1,14 @@
 package task6
 
 import (
-	"strconv"
 	"math"
-	"strings"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func DoTask6(q int, sq int) {
-	str := getSequence(q,sq)
+	str := getSequence(q, sq)
 	file, _ := os.Create("number sequence")
 	defer file.Close()
 	file.WriteString(str)
@@ -19,14 +19,14 @@ func getSequence(quantity int, square int) string {
 	root := int(math.Floor(math.Sqrt(float64(square))))
 
 	for root*root < square {
-		root+=1
+		root += 1
 	}
 
-	for i:=0; i < quantity; i++ {
+	for i := 0; i < quantity; i++ {
 		if root*root >= square {
 			file = append(file, string(strconv.Itoa(root)))
 		}
-		root +=1
+		root += 1
 	}
 	f := strings.Join(file, ", ")
 	return f
