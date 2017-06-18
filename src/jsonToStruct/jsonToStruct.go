adaf69bb1e3fe0c09dadc06063850fe12f27de5b
Evgeniy Kostrika <evgeniy.kostrika@gmail.com>
2017-06-18 21:04:16 +0300

    cool commit!

--- src/jsonToStruct/jsonToStruct.go
+++ src/jsonToStruct/jsonToStruct.go
@@ -4,42 +4,27 @@ import (
 	t2 "task2"
 	t3 "task3"
 )
-
-type Task1 struct {
-	Width, Height int
-	Symbol string
-}
-
-type Task2 struct {
-	Env1, Env2 t2.Envelope
-}
-
-type Task3 struct {
-	SliceOfTriangles []t3.Triangle
-}
-
-type Task4 struct {
-	Number int
-}
-
-type Task5 struct {
-	Min, Max int
-}
-
-type Task6 struct {
-	Length, MaxSquare int
-}
-
-type Task7 struct {
-	File string
-}
-
 type Data struct {
-	t1 *Task1
-	t2 *Task2
-	t3 *Task3
-	t4 *Task4
-	t5 *Task5
-	t6 *Task6
-	t7 *Task7
+	T1 struct {
+		Width, Height int
+		Symbol string
+	}
+	T2 struct {
+		Env1, Env2 t2.Envelope
+	}
+	T3 struct {
+		SliceOfTriangles []t3.Triangle
+	}
+	T4 struct {
+		Number int
+	}
+	T5 struct {
+		Min, Max int
+	}
+	T6 struct {
+		Length, MaxSquare int
+	}
+	T7 struct {
+		File string
+	}
 }
