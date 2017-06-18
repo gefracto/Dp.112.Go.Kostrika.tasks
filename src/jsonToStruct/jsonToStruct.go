4c5b0c6e1930a1660c48e2c1ac184281c5fb920d
Evgeniy Kostrika <evgeniy.kostrika@gmail.com>
2017-06-18 20:30:21 +0300

    cool commit!

--- src/jsonToStruct/jsonToStruct.go
+++ src/jsonToStruct/jsonToStruct.go
@@ -1,4 +1,32 @@
 package jsonToStruct
 
+type Task1 struct {
+}
+
+type Task2 struct {
+}
+
+type Task3 struct {
+}
+
+type Task4 struct {
+}
+
+type Task5 struct {
+}
+
+type Task6 struct {
+}
+
+type Task7 struct {
+}
+
 type Data struct {
+	t1 *Task1
+	t2 *Task2
+	t3 *Task3
+	t4 *Task4
+	t5 *Task5
+	t6 *Task6
+	t7 *Task7
 }
