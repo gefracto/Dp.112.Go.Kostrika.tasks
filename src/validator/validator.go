a2466737a4e2c3837dd0bfc4e6f08fe206a0d6c7
Evgeniy Kostrika <evgeniy.kostrika@gmail.com>
2017-06-19 03:08:48 +0300

    Parses both json and xml

--- src/validator/validator.go
+++ src/validator/validator.go
@@ -2,10 +2,11 @@ package validator
 
 import (
 	"fmt"
-	jsn "jsonToStruct"
+	parse "parseJsonOrXml"
+	"task1"
 )
 
-func Task1Rules(d jsn.Data) (ok bool, reason string) {
+func Task1Rules(d parse.Data) (ok bool, reason string) {
 	ok = true
 	if d.T1.Height <= 0 || d.T1.Width <= 0 {
 		reason = "Sides must be integers"
@@ -13,35 +14,36 @@ func Task1Rules(d jsn.Data) (ok bool, reason string) {
 	}
 	return
 }
-func Task2Rules(d jsn.Data) (ok bool, reason string) {
+func Task2Rules(d parse.Data) (ok bool, reason string) {
 	ok = true
 	return
 }
-func Task3Rules(d jsn.Data) (ok bool, reason string) {
+func Task3Rules(d parse.Data) (ok bool, reason string) {
 	ok = true
 	return
 }
-func Task4Rules(d jsn.Data) (ok bool, reason string) {
+func Task4Rules(d parse.Data) (ok bool, reason string) {
 	ok = true
 	return
 }
-func Task5Rules(d jsn.Data) (ok bool, reason string) {
+func Task5Rules(d parse.Data) (ok bool, reason string) {
 	ok = true
 	return
 }
-func Task6Rules(d jsn.Data) (ok bool, reason string) {
+func Task6Rules(d parse.Data) (ok bool, reason string) {
 	ok = true
 	return
 }
-func Task7Rules(d jsn.Data) (ok bool, reason string) {
+func Task7Rules(d parse.Data) (ok bool, reason string) {
 	ok = true
 	return
 }
 
 func RiseAndShine() {
-	Data := jsn.GetData()
+	Data := parse.GetData("data.json")
 	if ok, reason := Task1Rules(Data); ok {
 		//dowork
+		fmt.Println(task1.DoTask1(Data.T1.Width, Data.T1.Height, Data.T1.Symbol))
 	} else {
 		//dontwork
 		fmt.Println(ok, reason)
