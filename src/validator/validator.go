98ef7578e48fd9f703bd9334d369123c399bc01e
Evgeniy Kostrika <evgeniy.kostrika@gmail.com>
2017-06-21 01:04:09 +0300

    Work in progress!

--- src/validator/validator.go
+++ src/validator/validator.go
@@ -10,6 +10,7 @@ func typeof(v interface{}) string {
 	return fmt.Sprintf("%T", v)
 }
 //T1Width16Height8Symbol*T2Env1Side121Side215Env2Side13.5Side222.3T3SliceOfTrianglesNameABCA10.3B10.1C10NameDEFA10.1B10C10.3NameGHIA34B43.2C20NameJKLA2B3C2T4Number41212226T5Min999990Max999999T6Length10MaxSquare25T7Filecontext
+//21Side215Env2Side13.5Side222.3T3SliceOfTrianglesNameABCA10.3B10.1C10NameDEFA10.1B10C10.3NameGHIA34B43.2C20NameJKLA2B3C2T4Number12345T5Min999990Max999999T6Length10MaxSquare25T7Filecontext
 
 func Task1Rules(str string) (ok bool, reason string, file string) {
 	ok = true
@@ -34,10 +35,53 @@ func Task1Rules(str string) (ok bool, reason string, file string) {
 	// Symbol не проверяем
 	beginFileWithIndex := strings.Index(str, "Env1Side1") + len("Env1Side1")
 	file = string(str[beginFileWithIndex:])
+	fmt.Println(file)
 	return
 }
-func Task2Rules(s string) (ok bool, reason string, file string) {
+func Task2Rules(str string) (ok bool, reason string, file string) {
 	ok = true
+
+	rightEnv1Side1 := strings.Index(str, "Side2")
+
+	if _, err := strconv.ParseFloat(string(str[:rightEnv1Side1]), 64); err != nil {
+		ok = false
+		reason = "Значение Env1.Side1 не является float64"
+	}
+
+	leftEnv1Side2 := strings.Index(str, "Side2") + len("Side2")
+	rightEnv1Side2 := strings.Index(str, "Env2")
+
+	if _, err := strconv.ParseFloat(string(str[leftEnv1Side2:rightEnv1Side2]), 64); err != nil {
+		ok = false
+		reason = "Значение Env1.Side2 не является float64"
+	}
+
+	leftEnv2Side1 := strings.Index(str, "Env2Side1") + len("Env2Side1")
+	var rightEnv2Side1 int
+	for i:= leftEnv2Side1; i<len(str); i++ {
+		if string(str[i]) == "S" {
+			rightEnv2Side1 = i
+		}
+	}
+
+	if _, err := strconv.ParseFloat(string(str[leftEnv2Side1:rightEnv2Side1]), 64); err != nil {
+		ok = false
+		reason = "Значение Env2.Side1 не является float64"
+	}
+	//str2 := string(str[rightEnv2Side1:])
+
+
+	//leftEnv2Side2 := strings.Index(str2, "Side2") + len("Side2")
+	//rightEnv2Side2 := strings.Index(str2, "T3")
+
+	//if _, err := strconv.ParseFloat(string(str2[leftEnv2Side2:rightEnv2Side2]), 64); err != nil {
+	//	ok = false
+	//	reason = "Значение Env2.Side2 не является float64"
+	//}
+
+	//beginFileWithIndex := strings.Index(str2, "T3SliceOfTrianglesName") + len("T3SliceOfTrianglesName")
+	//file = string(str2[beginFileWithIndex:])
+	//fmt.Println(file)
 	return
 }
 func Task3Rules(s string) (ok bool, reason string, file string) {
@@ -56,8 +100,13 @@ func Task6Rules(s string) (ok bool, reason string, file string) {
 	ok = true
 	return
 }
-func Task7Rules(s string) (ok bool, reason string) {
+func Task7Rules(str string) (ok bool, reason string) {
 	ok = true
+/*	if string(str[strings.Index(str,"T7FileContex")]) != "T7FileContext" {
+		ok = false
+		reason = "Файл должен называться \"context\""
+	}
+*/
 	return
 }
 func JsonToStr(file string) (newFile string) {
