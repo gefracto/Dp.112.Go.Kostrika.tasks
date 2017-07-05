73d341e86dd7d16b59f98e41b918c8e1d442fcf4
gefracto <evgeniy.kostrika@gmail.com>
2017-07-05 13:02:41 +0300

    client added...

--- src/task2/task2.go
+++ src/task2/task2.go
@@ -85,24 +85,3 @@ func (e *Envelope) goesIn(e2 *Envelope) bool {
 	y := e2.diagonal()-e.biggerS() >= e.smallerS()
 	return x && y
 }
-
-// func dotask(e1 Envelope, e2 Envelope) (err error, data int) {
-// 	A, B := e1.Width, e1.Height
-// 	C, D := e2.Width, e2.Height
-
-// 	areEqual := (A == C || A == D) && (B == C || B == D)
-
-// 	if A <= 0 || B <= 0 || C <= 0 || D <= 0 {
-
-// 		return errors.New("Ошибка!\nСторона конверта " +
-// 			"не может быть меньше или равно 0"), 0
-
-// 	} else if areEqual {
-// 		return nil, 0
-
-// 	} else if e1.goesIn(&e2) {
-// 		return nil, 1
-
-// 	}
-// 	return nil, 2
-// }
