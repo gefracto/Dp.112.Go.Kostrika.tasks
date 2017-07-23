fc6904b5f463109de66a40b33738b3f99c864c1b
rey <rey@pc>
2017-07-23 12:41:57 +0300

    tests

---
+++ src/server/server_test.go
@@ -0,0 +1,16 @@
+package server
+
+import "testing"
+
+func Test_tetData(t *testing.T) {
+
+}
+func Test_IndexHandler(t *testing.T) {
+
+}
+func Test_HandleTasks(t *testing.T) {
+
+}
+func Test_HandleTask(t *testing.T) {
+
+}
