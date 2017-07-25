2e51a8f273a2b2737e5a281f5dd100b41fab3e43
gefracto <yevgeniy.kostrika@gmail.com>
2017-07-25 09:24:25 +0300

    tests

--- src/server/server_test.go
+++ src/server/server_test.go
@@ -1,12 +1,32 @@
 package server
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
-func Test_tetData(t *testing.T) {
+func Test_getData(t *testing.T) {
 
 }
 func Test_IndexHandler(t *testing.T) {
+	req, err := http.NewRequest("POST", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandleFunc(IndexHandler)
+	handler.ServeHTTP(rr, req)
 
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+	expected := `{"alive": true}`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
 }
 func Test_HandleTasks(t *testing.T) {
 
