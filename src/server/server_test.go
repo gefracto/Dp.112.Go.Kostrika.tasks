d85f2786f7901d26f9cfb46838558c1136eef3e1
gefracto <yevgeniy.kostrika@gmail.com>
2017-07-25 10:00:12 +0300

    tests

--- src/server/server_test.go
+++ src/server/server_test.go
@@ -39,10 +39,4 @@ func Test_HandleTask(t *testing.T) {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusOK)
 	}
-
-	expected := `{"alive": true}`
-	if rr.Body.String() != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v",
-			rr.Body.String(), expected)
-	}
 }
