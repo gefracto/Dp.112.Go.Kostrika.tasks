f8a4d491dd87967cc213b1d18aa50f475816fa00
gefracto <yevgeniy.kostrika@gmail.com>
2017-07-25 09:54:09 +0300

    tests

--- src/server/server_test.go
+++ src/server/server_test.go
@@ -27,5 +27,22 @@ func Test_HandleTasks(t *testing.T) {
 
 }
 func Test_HandleTask(t *testing.T) {
+	req, err := http.NewRequest("POST", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(IndexHandler)
+	handler.ServeHTTP(rr, req)
 
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := `{"alive": true}`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
 }
